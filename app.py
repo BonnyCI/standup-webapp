9235e5fd5e322084ddf6f63237031e3c94a40ac8
Bradon Kanyid <bradon@kanyid.org>
2017-04-04 13:54:56 -0700

    Add basic functionality
    The webapp now lists standups per-day, and has minimal navigation for
    days. It also has a bit of logic to skip weekends in navigation.
    
    Next, I need to fix index page, and make a better external
    configuration.
    
    Signed-off-by: Bradon Kanyid <bradon@kanyid.org>

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
-from flask import Flask, g, render_template
+from datetime import date, timedelta, datetime
+from flask import Flask, g, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -8,13 +9,20 @@ app.config.update(dict(
     DATABASE='/home/bradonk/code/errbot-testing/data/plugins/standup.sqlite'
 ))
 
-@app.route('/')
-def show_entries():
+# This helper is to simplify some logic
+@app.route('/show')
+def show():
+    date_str = request.args.get('date', '')
+    date_to_show = datetime.strptime(date_str,'%Y-%m-%d').date()
+    return redirect(url_for('show_entries', year=date_to_show.year, month=date_to_show.month, day=date_to_show.day))
+
+@app.route('/<year>/<month>/<day>/')
+def show_entries(year, month, day):
+    date_to_show, prior_date, next_date = get_dates(year, month, day)
     db = get_db()
-    cur = db.execute('select id, date, author, yesterday, today, blockers from statuses order by id desc')
+    cur = db.execute('select id, author, yesterday, today, blockers from statuses where date=? order by id desc', (date_to_show, ))
     entries = cur.fetchall()
-    print(entries)
-    return render_template('show_entries.html', entries=entries)
+    return render_template('show_entries.html', entries=entries, date_to_show=date_to_show.strftime("%A %Y-%m-%d"), prior_date=prior_date, next_date=next_date)
 
 def connect_db():
     """Connects to the specific database."""
@@ -32,3 +40,16 @@ def close_connection(exception):
     if hasattr(g, '_database'):
         g._database.close()
 
+def get_dates(year, month, day):
+    year, month, day = int(year), int(month), int(day)
+    date_to_show = date(year, month, day)
+    if date_to_show.weekday() > 0: # ignore weekend
+        prior_date = date_to_show - timedelta(days=1)
+    else:
+        prior_date = date_to_show - timedelta(days=3)
+
+    if date_to_show.weekday() < 4: #ignore weekend
+        next_date = date_to_show + timedelta(days=1)
+    else:
+        next_date = date_to_show + timedelta(days=3)
+    return (date_to_show, prior_date, next_date)
