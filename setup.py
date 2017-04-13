d1cea17f21150b2061f67992bd37388f2eae9d0f
Bradon Kanyid <bradon@kanyid.org>
2017-04-12 17:28:28 -0700

    Fix missing flag in setup.py to include data files
    Signed-off-by: Bradon Kanyid <bradon@kanyid.org>

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import setup, find_packages
 
 setup(name='standup_webapp',
       packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+          'flask',
+      ],
       version='0.1')
-
