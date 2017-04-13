db9d64d1fdf5a357a8a9cb83f45c83a4a19e301f
Bradon Kanyid <bradon@kanyid.org>
2017-04-12 17:52:42 -0700

    Add missing MANIFEST.in file to describe which data files to include
    Signed-off-by: Bradon Kanyid <bradon@kanyid.org>

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(name='standup_webapp',
       packages=find_packages(),
       include_package_data=True,
-      install_requires=[
-          'flask',
-      ],
+      install_requires=['Flask'],
+      zip_safe=False,
       version='0.1')
